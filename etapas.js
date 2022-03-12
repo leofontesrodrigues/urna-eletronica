b2b3ece1e16b7e6a8cdeefe8f1d1ff08b36493ea
leofontesrodrigues <96321175+leofontesrodrigues@users.noreply.github.com>
2022-03-12 20:21:14 -0300

    upload de arquivos

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [
+                    {url: '42_2.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos: [
+                    {url: '12345.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    {url: '42_2.jpg', legenda: 'Prefeito'},
+                    {url: '42.jpg', legenda: 'Vice-Prefeito', small: true}
+                ] 
+            },
+            {
+                numero: '84',
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos: [
+                    {url: '68.jpg', legenda: 'Prefeito'},
+                    {url: '68_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ] 
+            },
+        ]
+    }
+];
